868fe7e4b3da9e7dd7a11bb3e3a3aefa1f67d6d6
Frank Ma <ma990801@gmail.com>
2020-12-29 22:42:09 -0500

    Changed directory structure and names

--- step2mapper.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import sys
-
-for line in sys.stdin:
-    pagename_date, pageviews = line.strip().rsplit(" ", 1)
-    pagename, date = pagename_date.strip().rsplit("}", 1)
-
-    print(pagename + '\t' + date + '\t' + pageviews)
